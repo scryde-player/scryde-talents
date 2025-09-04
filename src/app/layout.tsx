d5cbe64395af38275c9e9dd02ac0f8e202263317
Scryde Player <username@users.noreply.github.com>
2025-09-04 19:49:17 +0300

    Добавил favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Scryde Talents",
   description: "by fans",
+  icons: {
+    icon: "/assets/images/fav.png",
+  },
 };
 
 export default function RootLayout({

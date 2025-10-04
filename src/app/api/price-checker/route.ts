fdfe51ab97f102b665f04e9eab4f5ac71a864f0b
Scryde Player <username@users.noreply.github.com>
2025-10-04 13:51:29 +0300

    feat: add deal_type

--- src/app/api/price-checker/route.ts
+++ src/app/api/price-checker/route.ts
@@ -12,6 +12,7 @@ const pool = new Pool({
 
 type Payload = {
   title: string;
+  type: string;
   price: string | number; // большие значения присылай строкой
   items_count: number;
   found_index?: number; // индекс в найденных данных
@@ -25,6 +26,7 @@ export async function POST(req: NextRequest) {
     // простая валидация
     items.forEach((it, i) => {
       if (typeof it.title !== "string") throw new Error("title must be string");
+      if (typeof it.type !== "string") throw new Error("type must be string");
       if (!["string", "number"].includes(typeof it.price))
         throw new Error("price must be string|number");
       if (typeof it.items_count !== "number")
@@ -38,8 +40,8 @@ export async function POST(req: NextRequest) {
     try {
       await client.query("BEGIN");
       const sql = `
-        INSERT INTO sell_data (title, price, items_count, found_index)
-        VALUES ($1, $2::NUMERIC, $3, $4)
+        INSERT INTO sell_data (title, price, items_count, found_index, deal_type)
+        VALUES ($1, $2::NUMERIC, $3, $4, $5)
       `;
       for (const it of items) {
         await client.query(sql, [
@@ -47,6 +49,7 @@ export async function POST(req: NextRequest) {
           String(it.price),
           it.items_count,
           it.found_index!,
+          it.type,
         ]);
       }
       await client.query("COMMIT");
